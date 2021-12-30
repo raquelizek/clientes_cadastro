415764079d8ef6c30eade4755c8fea7343870d50
raquelizek <raquelreclizek@gmail.com>
2021-12-30 16:22:44 -0300

    feat: translate the error messages

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Usuário ou senha incorreta.',
+    'password' => 'Usuário ou senha incorreta.',
+    'throttle' => 'Muitas tentativas. Tente novamente em :seconds segundos.',
 
 ];
