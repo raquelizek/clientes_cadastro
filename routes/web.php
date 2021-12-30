7e4cd17413e8bb7b56391875094091406617fa3d
raquelizek <raquelreclizek@gmail.com>
2021-12-30 16:23:43 -0300

    refactor: change controller name

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
-
-
 
 Route::get('/', function () {
     return view('auth.login');
@@ -12,9 +9,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/inicio', [App\Http\Controllers\CadastroUsuarioController::class, 'show']);
-Route::get('/cadastro-usuario', [App\Http\Controllers\CadastroUsuarioController::class, 'create']);
-Route::post('/salvarcliente', [App\Http\Controllers\CadastroUsuarioController::class, 'store']);
-Route::get('/usuarios-deletados/{id}', [App\Http\Controllers\CadastroUsuarioController::class, 'destroy']);
-Route::get('/cadastro-usuario/{id}/edit', [App\Http\Controllers\CadastroUsuarioController::class, 'edit']);
-Route::put('/cadastro-usuario/{id}/update', [App\Http\Controllers\CadastroUsuarioController::class, 'update']);
+Route::get('/inicio', [App\Http\Controllers\ClienteController::class, 'show']);
+Route::get('/cadastro-usuario', [App\Http\Controllers\ClienteController::class, 'create']);
+Route::post('/salvarcliente', [App\Http\Controllers\ClienteController::class, 'store']);
+Route::get('/usuarios-deletados/{id}', [App\Http\Controllers\ClienteController::class, 'destroy']);
+Route::get('/cadastro-usuario/{id}/edit', [App\Http\Controllers\ClienteController::class, 'edit']);
+Route::put('/cadastro-usuario/{id}/update', [App\Http\Controllers\ClienteController::class, 'update']);
